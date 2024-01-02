2c65ea6ed841eaa4bbc9c4a923222583fcf4e355
Your Name <you@example.com>
2024-01-02 20:24:31 +0200

    add custom item book & add custom book list view

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,4 +1,5 @@
 class AssetsData {
   static const splashLogo = 'assets/images/splash_image.png';
   static const logo = 'assets/images/Logo.png';
+  static const itemImage = 'assets/images/Book 1 High.png';
 }
--- lib/feature/home/presentation/view/widget/home_screen_body.dart
+++ lib/feature/home/presentation/view/widget/home_screen_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/feature/home/presentation/view/widget/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 
@@ -10,8 +11,45 @@ class HomeScreenBody extends StatelessWidget {
       child: Column(
         children: [
           CustomAppBar(),
+          CustomBookListView(),
         ],
       ),
+    );
+  }
+}
+
+class CustomBookListViewItem extends StatelessWidget {
+  const CustomBookListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8),
+      height: MediaQuery.of(context).size.height * .25,
+      width: MediaQuery.of(context).size.height * .17,
+      decoration: BoxDecoration(
+        image: const DecorationImage(
+            image: AssetImage(AssetsData.itemImage), fit: BoxFit.fill),
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.white,
+      ),
+    );
+  }
+}
+
+class CustomBookListView extends StatelessWidget {
+  const CustomBookListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .25,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return const SizedBox(height: 200, child: CustomBookListViewItem());
+          }),
     );
   }
 }
