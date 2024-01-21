110a0074cff771d16e406923b0269dc240968acf
Your Name <you@example.com>
2024-01-21 11:46:13 +0200

    neweset books cubit done

--- lib/feature/home/presentation/view_model/newset_books/newset_books_cubit.dart
+++ lib/feature/home/presentation/view_model/newset_books/newset_books_cubit.dart
@@ -1,9 +1,24 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/feature/home/data/model/book/book.model.dart';
+import 'package:bookly_app/feature/home/data/repo/repo_home.dart';
 import 'package:equatable/equatable.dart';
 
 part 'newset_books_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit() : super(NewsetBooksInitial());
+  NewsetBooksCubit(this.homeRepo) : super(NewsetBooksInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchNewesetBooks() async {
+    emit(NewsetBooksLoading());
+    var result = await homeRepo.fetchBooksHome();
+    result.fold((failure) {
+      emit(
+        NewsetBooksFailuer(errMessage: failure.errMessage),
+      );
+    }, (bookModel) {
+      emit(
+        NewsetBooksSuccess(booksModel: bookModel),
+      );
+    });
+  }
 }
