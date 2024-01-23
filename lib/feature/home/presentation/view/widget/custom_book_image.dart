ef8e0960d2ab37ce10d2e1e2265eab319abb9c6f
Your Name <you@example.com>
2024-01-23 11:13:28 +0200

    display feature list viewpart 2

--- lib/feature/home/presentation/view/widget/custom_book_image.dart
+++ lib/feature/home/presentation/view/widget/custom_book_image.dart
@@ -1,8 +1,8 @@
-import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
 class CustomBookImage extends StatelessWidget {
-  const CustomBookImage({super.key});
+  const CustomBookImage({super.key, required this.imageUrl});
+  final String imageUrl;
 
   @override
   Widget build(BuildContext context) {
@@ -10,8 +10,8 @@ class CustomBookImage extends StatelessWidget {
       aspectRatio: 2.6 / 4,
       child: Container(
         decoration: BoxDecoration(
-          image: const DecorationImage(
-            image: AssetImage(AssetsData.itemImage),
+          image: DecorationImage(
+            image: NetworkImage(imageUrl),
             fit: BoxFit.fill,
           ),
           borderRadius: BorderRadius.circular(12),
